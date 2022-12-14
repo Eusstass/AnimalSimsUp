056f3b0d39038eb46d6ade34a899011629dfbda9
Eustass <fhom2001@gmail.com>
2022-12-14 08:19:50 +0100

    Richtig so erste commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace AnimalSimsUps
+namespace AnimalSimsUp
 {
     /// <summary>
     /// Interaction logic for App.xaml
