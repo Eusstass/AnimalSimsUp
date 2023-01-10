8fd391666a3b5b08f62c9bc499dd97184b6e352a
Eustass <fhom2001@gmail.com>
2023-01-10 09:38:25 +0100

    Klassendiagram

---
+++ Quokka.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalSimsUp
+{
+    public class Quokka : Tier        // So nen lach vieh was Timo wollte
+    {
+    }
+}
---
+++ Tier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimalSimsUp
+{
+    public class Tier
+    {
+        public int FutterValue;
+        public int SchlafenValue;
+        public int LiebeValue;
+        protected int Art;
+
+        public Gehege Gehege
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Futtern()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Streicheln()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Schlafen()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
