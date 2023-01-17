e5bfb00f07363d4be0f2a9e0d5d686129e5e5976
Jyuratodon <timo.metze@gmx.de>
2023-01-17 15:28:59 +0100

    Die Klassen und Verärbungsstruktur
    Es wurden die Klassen und die Allgemeine Vererbungsstruktur eingearbeitet, Außerdem wurden die Bilder für die Tiere eingefügt

---
+++ Klassen/Tiere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSimsUp.Klassen
+{
+    class Tiger: Tier
+    { }
+    class Giraffe : Tier 
+    { }
+    class Quokka : Tier
+    { }
+    class Opossom : Tier 
+    { }
+}
